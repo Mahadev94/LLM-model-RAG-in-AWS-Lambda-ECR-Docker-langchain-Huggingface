0676cb8afbeeaef38bca98231fd60142e244381d
Mahadev Kharmate <mahadev.mk294@gmail.com>
2024-04-26 14:36:20 +0530

    code added for llama2

--- main.py
+++ main.py
@@ -3,3 +3,25 @@ import json
 import sys
 import boto3
 print('imported successfully..........')
+
+promt='''
+You are smart Assistant so please let me know machine learning is smartest way?
+'''
+bedrock=boto3.client(service_name='bedrock-runtime')
+payload={
+
+}
+body=json.dumps(payload)
+model_id="meta.llama2-70b-chat-v1"
+
+response=bedrock.invoke_model(
+    body=body,
+    model_id=model_id,
+    accept="application/json",
+    content_type="application/json"
+)
+
+response_body=json.load(response.get("body").read())
+
+response_text=response_body["generation"]
+print(response_text)
